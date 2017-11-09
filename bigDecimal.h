de80cee1b09985f4a1f5632419224a12c819a648
chloeminkyung <chloeminkyung@gmail.com>
2017-11-10 00:22:26 +0800

    Update bigDecimal.h

--- bigDecimal.h
+++ bigDecimal.h
@@ -26,8 +26,7 @@ public:
 	BigDecimal(double d);
 	BigDecimal(const BigDecimal& bi);
 	~BigDecimal();
-
-	/* #### You need to implement from_string(const char*) and to_string(char*) methods. #### */
+	
 	/**
 	 * Method: void from_string(const char* str)
 	 * Description:
@@ -42,8 +41,124 @@ public:
 	 * 		to_string method will output the current number to str.
 	 */
 	void to_string(char* str);
+	
+	BigDecimal operator+() const;
+	BigDecimal operator+(const BigDecimal& bd) const;
+	BigDecimal operator+(int i) const;
+	BigDecimal operator+(float f) const;
+	BigDecimal operator+(double b) const;
+	friend BigDecimal operator+(int i, const BigDecimal& bd);
+	friend BigDecimal operator+(float f, const BigDecimal& bd);
+	friend BigDecimal operator+(double b, const BigDecimal& bd);
 
-	/* #### Please add your overloading methods below. #### */
+	BigDecimal operator-() const;
+	BigDecimal operator-(const BigDecimal& bd) const;
+	BigDecimal operator-(int i) const;
+	BigDecimal operator-(float f) const;
+	BigDecimal operator-(double b) const;
+	friend BigDecimal operator-(int i, const BigDecimal& bd);
+	friend BigDecimal operator-(float f, const BigDecimal& bd);
+	friend BigDecimal operator-(double b, const BigDecimal& bd);
+
+	BigDecimal operator*() const;
+	BigDecimal operator*(const BigDecimal& bd) const;
+	BigDecimal operator*(int i) const;
+	BigDecimal operator*(float f) const;
+	BigDecimal operator*(double b) const;
+	friend BigDecimal operator*(int i, const BigDecimal& bd);
+	friend BigDecimal operator*(float f, const BigDecimal& bd);
+	friend BigDecimal operator*(double b, const BigDecimal& bd);
+
+	/* presicion for this operator overloading should not be longer than the original two number. 
+	 * 		example: 1.23333/2, the result precision should be 5. */
+	BigDecimal operator/(const BigDecimal& bd) const;
+	BigDecimal operator/(int i) const;
+	BigDecimal operator/(float f) const;
+	BigDecimal operator/(double b) const;
+	friend BigDecimal operator/(int i, const BigDecimal& bd);
+	friend BigDecimal operator/(float f, const BigDecimal& bd);
+	friend BigDecimal operator/(double b, const BigDecimal& bd);
+
+	/* both BigDecimal = BigDecimal and BigDecimal = int, float, double - return type BigDecimal& to support concatenation. */
+	BigDecimal& operator=(const BigDecimal& bd);
+	BigDecimal& operator=(int i);
+	BigDecimal& operator=(float f);
+	BigDecimal& operator=(double b);
+
+	BigDecimal& operator++();
+	BigDecimal operator++(int);
+
+	BigDecimal& operator--();
+	BigDecimal operator--(int);
+
+	bool operator>(const BigDecimal& bd) const;
+	bool operator>(int i) const;
+	bool operator>(float f) const;
+	bool operator>(double b) const;
+	friend bool operator>(int i, const BigDecimal& bd);
+	friend bool operator>(float f, const BigDecimal& bd);
+	friend bool operator>(double b, const BigDecimal& bd);
+
+	bool operator>=(const BigDecimal& bd) const;
+	bool operator>=(int i) const;
+	bool operator>=(float f) const;
+	bool operator>=(double b) const;
+	friend bool operator>=(int i, const BigDecimal& bd);
+	friend bool operator>=(float f, const BigDecimal& bd);
+	friend bool operator>=(double b, const BigDecimal& bd);
+
+	bool operator<(const BigDecimal& bd) const;
+	bool operator<(int i) const;
+	bool operator<(float f) const;
+	bool operator<(double b) const;
+	friend bool operator<(int i, const BigDecimal& bd);
+	friend bool operator<(float f, const BigDecimal& bd);
+	friend bool operator<(double b, const BigDecimal& bd);
+	
+	bool operator<=(const BigDecimal& bd) const;
+	bool operator<=(int i) const;
+	bool operator<=(float f) const;
+	bool operator<=(double b) const;
+	friend bool operator<=(int i, const BigDecimal& bd);
+	friend bool operator<=(float f, const BigDecimal& bd);
+	friend bool operator<=(double b, const BigDecimal& bd);
+
+	bool operator==(const BigDecimal& bd) const;
+	bool operator==(int i) const;
+	bool operator==(float f) const;
+	bool operator==(double b) const;
+	friend bool operator==(int i, const BigDecimal& bd);
+	friend bool operator==(float f, const BigDecimal& bd);
+	friend bool operator==(double b, const BigDecimal& bd);
+
+	bool operator!=(const BigDecimal& bd) const;
+	bool operator!=(int i) const;
+	bool operator!=(float f) const;
+	bool operator!=(double b) const;
+	friend bool operator!=(int i, const BigDecimal& bd);
+	friend bool operator!=(float f, const BigDecimal& bd);
+	friend bool operator!=(double b, const BigDecimal& bd);
+
+	/* presicion should not be longer than original two numbers.
+	 * 		example: base ^ power, base = BigDecimal, power = BigDecimal (converted into integer). */
+	BigDecimal operator^(const BigDecimal& bd) const;
+	BigDecimal operator^(int i) const;
+	BigDecimal operator^(float f) const;
+	BigDecimal operator^(double b) const;
+	friend BigDecimal operator^(int i, const BigDecimal& bd);
+	friend BigDecimal operator^(float f, const BigDecimal& bd);
+	friend BigDecimal operator^(double b, const BigDecimal& bd);
+
+	BigDecimal operator<<(int i);
+	BigDecimal operator<<(float f);
+	BigDecimal operator<<(double b);
+	friend ostream& operator<<(ostream& output, const BigDecimal& bd);
+
+	BigDecimal operator>>(int i);
+	BigDecimal operator>>(float f);
+	BigDecimal operator>>(double b);
+	friend istream& operator>>(istream& input, BigDecimal& bd);
+
 
 private:
 	Node* linkedList;
