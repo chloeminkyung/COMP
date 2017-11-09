41cada5dbd89f29c638b565d95d8e5756edfc69c
chloeminkyung <chloeminkyung@gmail.com>
2017-11-10 00:19:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 //==============================================================================
-// File: main.cpp
-// This is only a demo of BigDecimal test program. 
-// The one which grades your homework will be more complete and more strict.
+// Name        : main.cpp
+// Description : This is only a demo of BigDecimal test program. 
+// 	        	 The one which grades your homework will be more complete
+//	        	 and more strict.
 //==============================================================================
 
 #include "bigDecimal.h"
